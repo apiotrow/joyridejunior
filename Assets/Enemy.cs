28f2f40336619d22350801d8e2a9e18fee1b31b0
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2016-07-29 05:12:55 -0700

    A* pathfinding for enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : AILerp {
+	PlayerController pc;
+
+	void Start(){
+		base.Start();
+		base.ForceSearchPath();
+	}
+
+	public override void OnTargetReached () {
+		base.ForceSearchPath();
+	}
+
+}
---
+++ Assets/Scripts/BulletExplosion.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletExplosion : MonoBehaviour {
+	void Update () {
+		if(this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Done")){
+			Destroy(gameObject);
+		}
+	}
+}
