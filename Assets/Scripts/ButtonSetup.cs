a1978f410dc5d1c54a655aca3bba155c6afedeb1
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2016-08-01 11:06:09 -0700

    can find a random walkable node, reeeally inefficiently

--- Assets/Scripts/ButtonSetup.cs
+++ Assets/Scripts/ButtonSetup.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class ButtonSetup : MonoBehaviour {
+	Vector3 testPos;
 
 	void Start () {
 		Button btn;
@@ -11,9 +12,36 @@ public class ButtonSetup : MonoBehaviour {
 		btn.onClick.AddListener(() => {
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		});
+
+		StartCoroutine("nodes");
+	}
+
+	IEnumerator nodes()
+	{
+		while(true){
+			GameObject.Find("test").transform.position = testPos;
+
+			Pathfinding.NavGraph[] boo = GameObject.Find("A*").GetComponent<AstarPath>().astarData.graphs;
+
+			int randomNode = Random.Range(0, boo[0].CountNodes());
+
+			boo[0].GetNodes ((node) => {
+				
+				print("trying" + node.NodeIndex + ", " + randomNode);
+				if(node.NodeIndex == randomNode && node.Walkable == true){
+					print("test change");
+					var v = (Vector3)node.position;
+					testPos = v;
+					return false;
+				}
+				return true;
+			});
+
+			yield return new WaitForSeconds(1f);
+		}
 	}
 
 	void Update () {
-	
+
 	}
 }
